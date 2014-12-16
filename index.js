277d6774e5fa0a51de108905f6c942d7ff80f3e0
Vivek Kumar <vivekk.chugh@gmail.com>
2014-12-16 07:22:04 +0000

    updated index.js to push index.html

--- index.js
+++ index.js
@@ -4,8 +4,11 @@ var app = express();
 app.set('port', (process.env.PORT || 5000))
 app.use(express.static(__dirname + '/public'))
 
+var fs = require('fs');
+
 app.get('/', function(request, response) {
-  response.send('Hello World - who are you!')
+  output = fs.readFileSync('index.html').toString();
+  response.send(output);
 })
 
 app.listen(app.get('port'), function() {
